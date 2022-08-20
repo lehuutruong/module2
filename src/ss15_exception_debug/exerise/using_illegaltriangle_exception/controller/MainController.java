006029a7088c8a0d7674e6b6d558110bb21dd1ce
TRƯỜNG KHANG <lehuutruongkhang@gmail.com>
2022-08-20 11:16:47 +0700

    nộp bài tập

--- src/ss15_exception_debug/exerise/using_illegaltriangle_exception/controller/MainController.java
+++ src/ss15_exception_debug/exerise/using_illegaltriangle_exception/controller/MainController.java
@@ -7,14 +7,17 @@ import ss15_exception_debug.exerise.using_illegaltriangle_exception.service.impl
 import java.util.Scanner;
 
 public class MainController {
-    private Scanner scanner=new Scanner(System.in);
-    private ITriangleService iTriangleService=new TriangleService();
+    private Scanner scanner = new Scanner(System.in);
+    private ITriangleService iTriangleService = new TriangleService();
+
     public void running() {
-        int choose;
         while (true) {
-            mainMenu();
-            choose = Integer.parseInt(scanner.nextLine());
-            switch(choose) {
+            System.out.println("1. Tạo một hình tam giác");
+            System.out.println("2. Hiển thị danh sách các hình tam giác");
+            System.out.println("3. Thoát");
+            System.out.print("Chọn chức năng: ");
+            int choose = Integer.parseInt(scanner.nextLine());
+            switch (choose) {
                 case 1:
                     iTriangleService.addSidesOfTriangle();
                     break;
@@ -28,10 +31,5 @@ public class MainController {
             }
         }
     }
-    public void mainMenu() {
-        System.out.println("1. Tạo một hình tam giác");
-        System.out.println("2. Hiển thị danh sách các hình tam giác");
-        System.out.println("3. Thoát");
-        System.out.print("Chọn chức năng: ");
-    }
+
 }
